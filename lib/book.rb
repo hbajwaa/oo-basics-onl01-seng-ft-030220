361a13468898292b8651237819561e5563b787eb
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-12 08:05:20 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,10 +2,14 @@ class Book
   def initialize(title)
     @title = title
   end
-  
   def title
     @title
   end
+  
+  def author=(author)
+    @author = author
+  end
+  
   
 end
 Book.new("And Then There Were None")
